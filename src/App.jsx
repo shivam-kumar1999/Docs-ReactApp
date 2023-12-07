433037065c3dfd6b4acb2c7bfeef9bcb35b3d716
Shivam kumar <shivamkumarhost@gmail.com>
2023-12-07 22:05:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+  return (
+    
+          <div className="relative w-full h-screen bg-zinc-800">
+             <div className="w-full py-10 flex justify-center absolute top-[5%] text-200 text-white font-semibold text-xl">Documents</div>
+             <h1 className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] font-semibold text-9xl text-white leading-none tracking-tight">Docs.</h1>
+          </div>      
+   
+  )
+}
+
+export default App
